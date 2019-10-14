2a7d4bb6c93d75af82c52474853d0857dfb4233a
Patrick Lyden <syncc12@verizon.net>
2019-10-14 17:13:22 +0000

    Improved the design of the page.

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -64,6 +64,7 @@ $(function() {
         var ulTodos = $('.todo-list');
         ulTodos.append(htmlString);
         $('.toggle').click(toggleTask);
+        $('.new-todo').val('');
       });
     });
 
